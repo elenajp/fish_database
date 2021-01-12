e9c61215628349b8aab592b90481306d27007c6d
elena perez <elenaperez@ejperezk.home>
2021-01-12 20:58:46 +0100

    added function to insert data into db

--- db_con.py
+++ db_con.py
@@ -1,19 +1,30 @@
 import psycopg2
 
+image = open('/Users/elenaperez/Desktop/code/fish_images/clownfish.jpg', 'rb').read()
+
 def db_connection():
     try:
-        con = psycopg2.connect(
+        connection = psycopg2.connect(
             host = 'localhost',
             dbname = 'postgres',
             user = 'postgres',
             password = 'passwordd',
             port = '5432')
         print('You have connected to the db')
-        return con
+        return connection
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     return False
 
-    con.close()
 
+def insert_data():
+    cur = connection.cursor()
+    cur.execute("INSERT INTO sealife.sealife_data (name, description, image) values ('clown fish', 'I live in an anemone', psycopg2.Binary(image) )")
+
+    connection.commit()
+    cur.close()
+
+
+connection = db_connection()
 db_connection()
+insert_data()
