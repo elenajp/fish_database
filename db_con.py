67f7fae67e546bda6f570204a09e066ee701046b
elena perez <ejperezk@gmail.com>
2021-01-19 15:28:43 +0100

    imported env_file to make sure env variables were working and can actually insert data into the db

--- db_con.py
+++ db_con.py
@@ -1,36 +1,37 @@
 import psycopg2
 import os
+import env_file
+env_file.load('.env')
 
-image = open('/Users/elenaperez/Desktop/code/fish_images/clownfish.jpg', 'rb').read()
+#image = open('/Users/elenaperez/Desktop/code/fish_images/clownfish.jpg', 'rb').read()
 
 def db_connection():
     try:
-        connection = psycopg2.connect(
-            HOST = os.get('HOST'),
-            DBNAME = os.get('DBNAME'),
-            dbname = 'fish_images',
-            user = 'postgres',
-            password = 'passwordd',
-            port = '5432')
-        print('HOST')
+        con = psycopg2.connect(
+            host = os.getenv('HOST'),
+            dbname = os.getenv('DBNAME'),
+            user = os.getenv('USER'),
+            password = os.getenv('PASSWORD'),
+            port = os.getenv('PORT')
+        )
         print('You have connected to the db')
-        return connection
+        return con
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     return False
 
 
 def insert_data():
-    cur = connection.cursor()
-    cur.execute("INSERT INTO sealife.sealife_data (name, description) values ('clown fish', 'I live in an anemone')")
+    cur = con.cursor()
+    cur.execute("INSERT INTO sealife.sealife_data (name, description) VALUES ('clown fish', 'I live in an anemone')")
 
-    connection.commit()
-    cur.close()
+    con.commit()
+    cur.close()    
 
 
-connection = db_connection()
+con = db_connection()
 insert_data()
+con.close()
 
 db_connection()
-
 #psycopg2.Binary(image)
