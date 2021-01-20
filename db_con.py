64a288b7a755e96aac85451b42d05b4b35925222
elena perez <ejperezk@gmail.com>
2021-01-20 23:14:26 +0100

    converted image to string using base64

--- db_con.py
+++ db_con.py
@@ -1,18 +1,32 @@
-import psycopg2
+import base64
 import os
+
 import env_file
+import psycopg2
+
+# from config import config
 env_file.load('.env')
 
-#image = open('/Users/elenaperez/Desktop/code/fish_images/clownfish.jpg', 'rb').read()
+
+def image_to_base64():
+    global image_string
+    with open("/Users/elenaperez/Desktop/code/fish_images/clownfish.jpg", "rb") as img_file:
+        image_string = base64.b64encode(img_file.read())
+    return image_string
+
+
+image_to_base64()
+
 
 def db_connection():
     try:
         con = psycopg2.connect(
-            host = os.getenv('HOST'),
-            dbname = os.getenv('DBNAME'),
-            user = os.getenv('USER'),
-            password = os.getenv('PASSWORD'),
-            port = os.getenv('PORT')
+            host=os.getenv('HOST'),
+            dbname=os.getenv('DBNAME'),
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            port=os.getenv('PORT')
+
         )
         print('You have connected to the db')
         return con
@@ -23,15 +37,18 @@ def db_connection():
 
 def insert_data():
     cur = con.cursor()
-    cur.execute("INSERT INTO sealife.sealife_data (name, description) VALUES ('clown fish', 'I live in an anemone')")
+    cur.execute(
+        "INSERT INTO sealife.sealife_data (name, description) VALUES (%s, %s, %s)",
+        ('clown fish', 'I live in an anemone', image_string))
 
     con.commit()
-    cur.close()    
+    cur.close()
 
 
+x = image_to_base64()
 con = db_connection()
 insert_data()
 con.close()
 
 db_connection()
-#psycopg2.Binary(image)
+# psycopg2.Binary(image)
